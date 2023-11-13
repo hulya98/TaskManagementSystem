62b15aac7932eaf38f87591bf8b876cc8ceee524
Hulya <hulya.garibli@apertech.net>
2023-11-13 17:55:30 +0400

    tasks added

--- src/main/kotlin/com/taskManagementSystem/demo/entity/Task.kt
+++ src/main/kotlin/com/taskManagementSystem/demo/entity/Task.kt
@@ -7,13 +7,12 @@ import jakarta.persistence.Table
 import org.hibernate.annotations.GenericGenerator
 
 @Entity
-@Table(name="users")
-class User (
+@Table(name = "tasks")
+class Task(
     @Id
     @GeneratedValue(generator = "UUID")
     @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
-    val id:String,
-    val firstName:String,
-    val lastName:String
-
+    val id: String,
+    val taskHeader: String,
+    val taskDescription: String
 )
